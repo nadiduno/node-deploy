e577ecd26dbaddce9bc1b4592c6a50e9b0cb59fd
nadiduno <nadiduno@gmail.com>
2024-05-25 13:51:26 -0300

    Add endpoint  UPDATE and  DELETE

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient, User } from '@prisma/client'
 import fastify from 'fastify'
 import { z } from 'zod'
 
@@ -6,12 +6,20 @@ const app = fastify()
 
 const prisma = new PrismaClient()
 
+interface UserParams {
+  name: string;
+  email: string;
+}
+
+//READ
 app.get('/users', async () => {
   const users = await prisma.user.findMany()
 
   return { users }
 })
 
+
+//CREATE
 app.post('/users', async (request, reply) => {
   const createUserSchema = z.object({
     name: z.string(),
@@ -29,6 +37,33 @@ app.post('/users', async (request, reply) => {
 
   return reply.status(201).send()
 })
+
+
+
+//UPDATE
+app.put('/users/:id', async (request, reply) => {
+  const { id } = request.params as { id: string };
+  const { name, email } = request.body as UserParams;
+
+  const updatedUser = await prisma.user.update({
+    where: { id: id }, 
+    data: { name, email },
+  });
+
+  return reply.status(200).send(updatedUser);
+});
+
+//DELETE
+app.delete('/users/:id', async (request, reply) => {
+  const { id } = request.params as { id: string };
+
+  const deletedUser = await prisma.user.delete({
+    where: { id: id }, 
+  });
+
+  return reply.status(200).send(deletedUser);
+});
+
 
 app.listen({
   host: '0.0.0.0',
